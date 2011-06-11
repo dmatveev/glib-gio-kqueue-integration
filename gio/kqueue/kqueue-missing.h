922806d6fb6699c3a5145daa7b628f2db04d595b
Dmitry Matveev <me@dmitrymatveev.co.uk>
2011-06-11 22:21:22 +0000

    If needed, remove the subscription entry from a list of missing files on monitoring cancellation

--- gio/kqueue/kqueue-missing.h
+++ gio/kqueue/kqueue-missing.h
@@ -5,5 +5,6 @@ typedef void (*on_create_cb) (kqueue_sub *);
 
 void _km_init        (on_create_cb cb);
 void _km_add_missing (kqueue_sub *sub);
+void _km_remove      (kqueue_sub *sub);
 
 #endif /* __G_KQUEUE_MISSING_H */
